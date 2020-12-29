bba13bae4333911d90da1b3cb622f717524bb38d
krothenbaum <krothenbaum@gmail.com>
2020-12-28 17:09:03 -0800

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const IndexPage: FC = () => {
+  return <h1 className="text-blue-500">Hello, World!</h1>;
+};
+
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  important: true,
+  purge: {
+    content: ["./components/**/*.tsx", "./pages/**/*.tsx"],
+  },
+  theme: {},
+  variants: {},
+  plugins: [],
+};
